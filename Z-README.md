25278e22513ce2c93c933d0e8a92f4c24ac1ce60
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-11 08:21:18 -0600

    Added introduction to react router, specifying route paths, and custom 404 page

--- Z-README.md
+++ Z-README.md
@@ -1,3 +1,5 @@
 # React Router Course
 
 [Link][https://www.youtube.com/watch?v=oTIJunBa6MA]
+
+This is going to be used to practice client-side routing in a React app
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import HomePage from './pages/HomePage.tsx'
+import ProfilesPage from './pages/ProfilesPage.tsx'
+import ProfilePage from './pages/ProfilePage.tsx'
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <HomePage/>,
+  errorElement: <h1>404, Page not found</h1>
+  },
+  {
+    path: '/profiles',
+    element: <ProfilesPage/>
+  },
+  {
+    path: '/profile',
+    element: <ProfilePage/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
